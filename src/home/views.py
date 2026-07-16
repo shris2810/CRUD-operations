5d532a69fa589b770d7a787c11423f88fc88e49c
Srishti Malik <srishti.malik2810@gmail.com>
2026-07-16 23:49:01 +0530

    Stage 1 : root and health enpoints

--- src/home/views.py
+++ src/home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+def health(request):
+    return JsonResponse({"status": "ok"})
 
 def home(request):
-    return HttpResponse("Hello, Server")
+    return JsonResponse({ 
+        "name": "Task API", 
+        "version": "1.0", 
+        "endpoints": ["/tasks"] }
+    )

4f2ed0bb54d246d1471081115cbe3708da3e59f7
Srishti Malik <srishti.malik2810@gmail.com>
2026-07-18 21:04:36 +0530

    Stage 2: read endpoints with 404

--- src/home/urls.py
+++ src/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home,health
+from .views import home,health,get_tasks,get_task
 
 urlpatterns = [
-    path("", home),
-    path("health/", health),
+    path("", home, name="home"),
+    path("health/", health, name="health"),
+    path("tasks/", get_tasks, name="get_tasks"),
+    path("tasks/<int:task_id>/", get_task, name="get_task")
 ]
--- src/home/views.py
+++ src/home/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render
 
 from django.http import HttpResponse, JsonResponse
 
+tasks = [
+    {"id": 1, "title": "Task 1", "done": True},
+    {"id": 2, "title": "Task 2", "done": True},
+    {"id": 3, "title": "Task 3", "done": False},
+] # type: ignore
+
+def get_tasks(request):
+    return JsonResponse(tasks, safe=False)
+
+
+def get_task(request, task_id):
+    for task in tasks:
+        if task["id"] == task_id:
+            return JsonResponse(task)
+
+    return JsonResponse(
+        {"error": "Task not found"},
+        status=404
+    )
+
 def health(request):
     return JsonResponse({"status": "ok"})
 

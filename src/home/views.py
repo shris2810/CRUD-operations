e8557a0e3895b62efa1b522a6ae69697b2a9a828
Srishti Malik <srishti.malik2810@gmail.com>
2026-07-25 21:25:49 +0530

    create with validation

--- src/home/views.py
+++ src/home/views.py
@@ -1,14 +1,35 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-
+import json
 from django.http import HttpResponse, JsonResponse
 
 tasks = [
-    {"id": 1, "title": "Task 1", "done": True},
-    {"id": 2, "title": "Task 2", "done": True},
-    {"id": 3, "title": "Task 3", "done": False},
+    {"id": 1, "title": "Read book", "done": False},
+    {"id": 2, "title": "Exercise", "done": True},
 ] # type: ignore
 
+@csrf_exempt
 def get_tasks(request):
+    if(request.method == "POST"):
+        body = json.loads(request.body)
+        # give "" if no title present as default view and trims whitespace at end & begining
+        title = body.get("title","").strip()
+
+        if not title:
+            return JsonResponse(
+                {"error" : "title is required"},
+                status=400
+            )
+        
+        next_id = max((task["id"] for task in tasks), default=0) + 1
+        new_task = {
+            "id": next_id,
+            "title": title,
+            "done": False
+        }
+        tasks.append(new_task)
+        return JsonResponse(new_task, status=201)
+
     return JsonResponse(tasks, safe=False)
 
 

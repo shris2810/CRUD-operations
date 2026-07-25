48c7218d0e39b1920d6e9738eb7a7c3ee0d7c9d3
Srishti Malik <srishti.malik2810@gmail.com>
2026-07-25 22:13:24 +0530

    Stage 4: full CRUD

--- src/home/urls.py
+++ src/home/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import home,health,get_tasks,get_task
+from .views import home,health,task_detail,tasks_view_create
 
 urlpatterns = [
     path("", home, name="home"),
     path("health/", health, name="health"),
-    path("tasks/", get_tasks, name="get_tasks"),
-    path("tasks/<int:task_id>/", get_task, name="get_task")
+    path("tasks/", tasks_view_create, name="get_tasks"),
+    path("tasks/<int:task_id>/", task_detail, name="get_task"),
 ]
--- src/home/views.py
+++ src/home/views.py
@@ -9,7 +9,7 @@ tasks = [
 ] # type: ignore
 
 @csrf_exempt
-def get_tasks(request):
+def tasks_view_create(request):
     if(request.method == "POST"):
         body = json.loads(request.body)
         # give "" if no title present as default view and trims whitespace at end & begining
@@ -32,16 +32,47 @@ def get_tasks(request):
 
     return JsonResponse(tasks, safe=False)
 
-
-def get_task(request, task_id):
-    for task in tasks:
-        if task["id"] == task_id:
-            return JsonResponse(task)
-
-    return JsonResponse(
-        {"error": "Task not found"},
-        status=404
+@csrf_exempt
+def task_detail(request, task_id):
+    task = next(
+        (task for task in tasks if task["id"] == task_id),
+        None
     )
+
+    if not task:
+        return JsonResponse(
+            {"error": "Task not found"},
+            status=404
+        )
+
+    if(request.method == "GET"):
+        return JsonResponse(task)
+    
+    elif(request.method == "DELETE"):
+        tasks.remove(task)
+        return JsonResponse(
+            {},
+            status=204
+        )
+
+    elif(request.method == "PUT"):
+        body = json.loads(request.body)
+        if not body:
+            return JsonResponse(
+                {"error": "Request body cannot be empty"},
+                status=400
+            )
+        
+        title = body.get("title","").strip()
+        if title:
+            task["title"] = title
+
+        if "done" in body:
+            task["done"] = body["done"]
+
+        return JsonResponse(task)
+    
+
 
 def health(request):
     return JsonResponse({"status": "ok"})
